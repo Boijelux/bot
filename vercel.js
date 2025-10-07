6a24d49eea01cf7aa829fbacdc7719355857031c
Boijelux <boijelux@gmail.com>
2025-10-07 01:34:05 +0100

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,11 @@
+{
+  "symbol": "BTCUSDT",
+  "type": "spot-future",
+  "marketA": "https://api.bitget.com/api/spot/v1/market/ticker?symbol=BTCUSDT",
+  "marketB": "https://api.bitget.com/api/mix/v1/market/ticker?symbol=BTCUSDT_UMCBL",
+  "priceA": 67942.15,
+  "priceB": 67961.21,
+  "spreadAbs": 19.06,
+  "spreadPct": "0.0281",
+  "timestamp": 1728329131820
+}
